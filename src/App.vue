b45b30c091b06108a28e122f5b42237f1a89bba4
Carlos Sanjuan <carlosdsanjuan@outlook.com>
2022-11-18 15:13:18 -0500

    Test with expected functionality

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="h-screen w-full flex flex-col justify-center items-center p-10">
-    <article
-      class="bg-neutral-100 rounded-lg w-[1000px] h-fit overflow-y-auto"
-    >
-      <header class="p-4 flex justify-between items-center gap-12">
-        <h1 class="text-2xl font-bold text-neutral-800 text-center">
-          Busqueda en Github
-        </h1>
-        <button
-          class="bg-white uppercase font-bold px-5 py-3 rounded-lg inline-block hover:bg-neutral-200 ease-in-out transition-all duration-200"
-          @click="search"
-        >
-          Buscar
-        </button>
-      </header>
+  <div class="h-screen w-full grid grid-cols-1 xl:grid-cols-2 gap-12 p-10">
+    <article class="bg-neutral-100 rounded-lg w-full h-full overflow-y-auto">
       <form class="p-4" @submit.prevent="search">
+        <header class="p-4 flex justify-between items-center gap-12">
+          <h1 class="text-2xl font-bold text-neutral-800 text-center">
+            Busqueda en Github
+          </h1>
+          <button
+            type="submit"
+            class="bg-white uppercase font-bold px-5 py-3 rounded-lg inline-block hover:bg-neutral-200 ease-in-out transition-all duration-200"
+          >
+            Buscar
+          </button>
+        </header>
         <search-bar-component
           required
           :value="searchOptions.query"
@@ -48,9 +46,21 @@
           <repository-list-component :data="data" />
         </section>
         <section v-else class="my-5">
-          <p class="text-center text-neutral-800">No hay resultados</p>
+          <p class="text-center text-neutral-800 text-2xl">No hay resultados</p>
         </section>
       </Transition>
+    </article>
+    <article>
+      <h1 v-if="!data" class="text-3xl">
+        No hay suficientes resultados para graficar
+      </h1>
+      <section v-else>
+        <h1 class="text-3xl mb-5">Grafica de estrellas</h1>
+        <doughnut-component
+          :chart-data="chartData"
+          :chart-options="chartOptions"
+        />
+      </section>
     </article>
   </div>
 </template>
@@ -60,10 +70,12 @@
 import SearchBarComponent from "./components/SearchBarComponent.vue";
 // Importar componente de lista de repositorios
 import RepositoryListComponent from "./components/RepositoryListComponent.vue";
+// Importar componente de grafica de dona
+import DoughnutComponent from "./components/DoughnutComponent.vue";
 // Importar fragmentos de las consultas de GraphQL
 import { SEARCH_REPOS } from "@/graphql/documents";
 // Importar dependecias de Apollo y Vue
-import { reactive, ref } from "vue";
+import { reactive, ref, watch } from "vue";
 import { useQuery } from "@vue/apollo-composable";
 import { useApollo } from "@/composables/useApollo";
 
@@ -80,10 +92,27 @@ const searchOptions = reactive({
 // Variable reactiva que guarda la respuesta de la peticion
 const data = ref(null);
 
+// Variable reactiva que se pasa como prop al grafico de dona para mostrar los datos una vez la consulta se realiza
+let chartData = ref({
+  labels: [],
+  datasets: [
+    {
+      backgroundColor: ["#41B883", "#E46651", "#00D8FF", "#DD1B16"],
+      data: [],
+    },
+  ],
+});
+
+// Variable fija que se pasa como prop al grafico de dona para configurar la grafica
+let chartOptions = {
+  responsive: true,
+  maintainAspectRatio: false,
+};
+
 // Evento que se dispara al hacer click en el boton de busqueda
 async function search() {
-  if (searchOptions.query) {
-    // Se ejecuta la peticion de busqueda de repositorios
+  if (searchOptions.query !== "") {
+    // Se ejecuta la peticion de busqueda de repositorios a traves de useQuery
     const { onResult } = await useQuery(SEARCH_REPOS, {
       query: `
       ${searchOptions.query}
@@ -91,12 +120,26 @@ async function search() {
       `,
       limit: searchOptions.limit,
     });
+    // Con el hook onResult se obtiene la respuesta de la peticion
     onResult((queryResult) => {
       const { search } = queryResult.data;
       data.value = search.edges;
     });
   }
 }
+
+// Watcher que se utiliza para insertar de forma dinamica los datos de la respuesta de la peticion en la grafica
+watch(
+  () => data.value,
+  (newValue) => {
+    if (newValue) {
+      chartData.value.labels = newValue.map((item) => item.node.name);
+      chartData.value.datasets[0].data = newValue.map(
+        (item) => item.node.stargazers.totalCount
+      );
+    }
+  }
+);
 </script>
 
 <style scoped>
---
+++ src/components/DoughnutComponent.vue
@@ -0,0 +1,69 @@
+<template>
+  <doughnut
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    :chart-id="chartId"
+    :dataset-id-key="datasetIdKey"
+    :plugins="plugins"
+    :css-classes="cssClasses"
+    :styles="styles"
+    :width="width"
+    :height="height"
+  />
+</template>
+
+<script setup>
+// Importar dependecias de ChartJs para su funcionamiento
+import { Doughnut } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+  CategoryScale,
+} from "chart.js";
+
+// Definiendo props necesarias para la creación del gráfico recibiendo la data desde el padre y
+// asignando valores por defecto
+const props = defineProps({
+  chartId: {
+    type: String,
+    default: "doughnut-chart",
+  },
+  datasetIdKey: {
+    type: String,
+    default: "label",
+  },
+  width: {
+    type: Number,
+    default: 400,
+  },
+  height: {
+    type: Number,
+    default: 400,
+  },
+  cssClasses: {
+    default: "",
+    type: String,
+  },
+  styles: {
+    type: Object,
+    default: () => {},
+  },
+  plugins: {
+    type: Array,
+    default: () => [],
+  },
+  chartOptions: {
+    type: Object,
+    default: () => {},
+  },
+  chartData: {
+    type: Object,
+    default: () => {},
+  },
+});
+
+ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
+</script>
